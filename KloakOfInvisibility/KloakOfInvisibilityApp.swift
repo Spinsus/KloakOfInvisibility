73e8bec46929b44ea71cc6a1f1d6f49183258b87
Kevin L <kevinibex@gmail.com>
2025-07-28 20:29:32 -0400

    Initial Commit

---
+++ KloakOfInvisibility/KloakOfInvisibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  KloakOfInvisibilityApp.swift
+//  KloakOfInvisibility
+//
+//  Created by Kevo on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct KloakOfInvisibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
